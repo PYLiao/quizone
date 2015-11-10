cd4c8b821248d97757cc5aecf6a9017cdf8a8172
PYLiao <lby75429@gmail.com>
2015-11-10 06:24:30 +0000

    create submit function and return to root

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,9 +1,21 @@
 class TestsController < ApplicationController
-	def index
-		@tests = Test.all
-	end
+    def index
+        @tests = Test.all
+    end
 
-	def new
-		@test = Test.new
-	end
+    def new
+        @test = Test.new
+    end
+
+    def create
+    Test.create(test_params)
+    redirect_to root_path
+    end
+
+    private
+
+    def test_params
+        params.require(:test).permit(:name, :idea)
+    end
+
 end
